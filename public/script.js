6108c01011b5c769df7c885d976808b4c2433560
Nimira43 <n.rabone72@aol.com>
2025-12-14 03:44:13 +0000

    loadVoices function - 1

--- public/script.js
+++ public/script.js
@@ -2,6 +2,12 @@ const voiceSelect = document.querySelector('#voiceSelect')
 const playButton = document.querySelector('#playButton')
 const textInput = document.querySelector('textarea')
 
+let voices = []
+
+function loadVoices() {
+  
+}
+
 playButton.addEventListener('click', () => {
   const utterance = new SpeechSynthesisUtterance(textInput.value)
   speechSynthesis.speak(utterance)
