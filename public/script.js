5c57cbafef1f830b43490f13edbd88e1637e215d
Nimira43 <n.rabone72@aol.com>
2025-12-14 03:22:13 +0000

    playButton event listener

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
 const voiceSelect = document.querySelector('#voiceSelect')
 const playButton = document.querySelector('#playButton')
 const textInput = document.querySelector('textare')
+
+playButton.addEventListener('click', () => {
+  const utterance = new SpeechSynthesisUtterance(textInput.value)
+  speechSynthesis.speak(utterance)
+})
