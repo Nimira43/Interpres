07ee52c2011fa0520dc106a1fa4d0605b9b877f5
Nimira43 <n.rabone72@aol.com>
2025-12-31 11:58:13 +0000

    Moved the languages array into it's own file.

--- public/script.js
+++ public/script.js
@@ -1,39 +1,10 @@
+import { languages } from './js/languages.js'
+
 const voiceSelect = document.querySelector('#voiceSelect')
 const playButton = document.querySelector('#playButton')
 const textInput = document.querySelector('textarea')
 
-const languages = [
-  { code: 'en', name: 'English' },
-  { code: 'es', name: 'Spanish' },
-  { code: 'fr', name: 'French' },
-  { code: 'de', name: 'German' },
-  { code: 'it', name: 'Italian' },
-  { code: 'ur', name: 'Urdu' },
-  { code: 'ja', name: 'Japanese' },
-  { code: 'zh-CN', name: 'Chinese (Simplified)' },
-  { code: 'hne', name: 'Chhattisgarhi' },
-  { code: 'uk', name: 'Ukrainian' },
-  { code: 'bg', name: 'Bulgarian' },
-  { code: 'cy', name: 'Welsh' },
-  { code: 'gd', name: 'Scottish Gaelic' },
-  { code: 'ro', name: 'Romanian' },
-  { code: 'pt', name: 'Portuguese' },
-  { code: 'ru', name: 'Russian' },
-  { code: 'ar', name: 'Arabic' },
-  { code: 'hi', name: 'Hindi' },
-  { code: 'bn', name: 'Bengali' },
-  { code: 'ko', name: 'Korean' },
-  { code: 'sv', name: 'Swedish' },
-  { code: 'no', name: 'Norwegian' },
-  { code: 'fi', name: 'Finnish' },
-  { code: 'pl', name: 'Polish' },
-  { code: 'cs', name: 'Czech' },
-  { code: 'el', name: 'Greek' },
-  { code: 'tr', name: 'Turkish' },
-  { code: 'vi', name: 'Vietnamese' },
-  { code: 'nl', name: 'Dutch' },
-  { code: 'he', name: 'Hebrew' },
-]
+console.log(languages)
 
 let voices = []
 
